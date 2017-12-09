b1a04c01d4f84c943997dafc1917eba69b461813
Thiago <thia.mdossantos@gmail.com>
2017-12-09 21:06:38 -0200

    :sparkles: answer exercises

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -3,14 +3,6 @@ package i_introduction._4_Lambdas
 import util.TODO
 import util.doc4
 
-fun example() {
-
-    val sum = { x: Int, y: Int -> x + y }
-    val square: (Int) -> Int = { x -> x * x }
-
-    sum(1, square(2)) == 5
-}
-
 fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     """
         Task 4.
@@ -22,4 +14,4 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean = collection.any({ it % 2 == 0 })
