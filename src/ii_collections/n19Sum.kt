770674a1351c750aae64534f3189f0b7df98e78d
Thiago <thia.mdossantos@gmail.com>
2017-12-11 20:32:40 -0200

    resolve 19

--- src/ii_collections/n19Sum.kt
+++ src/ii_collections/n19Sum.kt
@@ -1,12 +1,3 @@
 package ii_collections
 
-fun example6() {
-    listOf(1, 3).sum() == 4
-    listOf("a", "b", "cc").sumBy { it.length } == 4
-}
-
-fun Customer.getTotalOrderPrice(): Double {
-    // Return the sum of prices of all products that a customer has ordered.
-    // Note: a customer may order the same product for several times.
-    todoCollectionTask()
-}
+fun Customer.getTotalOrderPrice(): Double = this.orders.flatMap { it.products }.sumByDouble { it.price }
