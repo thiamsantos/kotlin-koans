d4eb04dcf3c6b8cbc553a3876b27f4a7d3e27aff
Thiago <thia.mdossantos@gmail.com>
2017-12-11 20:00:53 -0200

    resolve 14

--- src/ii_collections/n14FilterMap.kt
+++ src/ii_collections/n14FilterMap.kt
@@ -1,21 +1,5 @@
 package ii_collections
 
-fun example1(list: List<Int>) {
+fun Shop.getCitiesCustomersAreFrom(): Set<City> = this.customers.map { it.city }.toSet()
 
-    // If a lambda has exactly one parameter, that parameter can be accessed as 'it'
-    val positiveNumbers = list.filter { it > 0 }
-
-    val squares = list.map { it * it }
-}
-
-fun Shop.getCitiesCustomersAreFrom(): Set<City> {
-    // Return the set of cities the customers are from
-    todoCollectionTask()
-}
-
-fun Shop.getCustomersFrom(city: City): List<Customer> {
-    // Return a list of the customers who live in the given city
-    todoCollectionTask()
-}
-
-
+fun Shop.getCustomersFrom(city: City): List<Customer> = this.customers.filter { it.city.equals(city) }
