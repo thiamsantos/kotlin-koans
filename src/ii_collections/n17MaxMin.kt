8489cd0b9c5af08112ae29e74232974e627651d5
Thiago <thia.mdossantos@gmail.com>
2017-12-11 20:27:06 -0200

    resolve 17

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -1,16 +1,5 @@
 package ii_collections
 
-fun example4() {
-    val max = listOf(1, 42, 4).max()
-    val longestString = listOf("a", "b").maxBy { it.length }
-}
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = this.customers.maxBy { it.orders.count() }
 
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
-    // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
-}
-
-fun Customer.getMostExpensiveOrderedProduct(): Product? {
-    // Return the most expensive product which has been ordered
-    todoCollectionTask()
-}
+fun Customer.getMostExpensiveOrderedProduct(): Product? = this.orders.flatMap { it.products }.maxBy { it.price }
