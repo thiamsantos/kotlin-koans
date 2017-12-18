539f3518cfc65fd2a6c03a255181a9fd6e22a6e7
Thiago <thia.mdossantos@gmail.com>
2017-12-18 11:30:36 -0200

    resolve 35

--- test/iv_properties/N35HowDelegatesWorkKtTest.kt
+++ test/iv_properties/N35HowDelegatesWorkKtTest.kt
@@ -5,12 +5,13 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class N35HowDelegatesWorkKtTest {
-    @Test fun testDate() {
+    @Test
+    fun testDate() {
         val d = D()
         /* Month numbering starts with 0 (0-Jan, 1-Feb, ... 11-Dec) */
         d.date = MyDate(2014, 1, 13)
-        assertEquals(2014, d.date.year)
-        assertEquals(1, d.date.month)
-        assertEquals(13, d.date.dayOfMonth)
+        assertEquals(2014, d.date?.year)
+        assertEquals(1, d.date?.month)
+        assertEquals(13, d.date?.dayOfMonth)
     }
 }
