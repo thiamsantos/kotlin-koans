a5214998ef30f6c6c74082fa7cafafc53941f7ef
Thiago <thia.mdossantos@gmail.com>
2017-12-18 11:38:08 -0200

    resolve 36

--- test/v_builders/N36ExtensionFunctionLiteralsKtTest.kt
+++ test/v_builders/N36ExtensionFunctionLiteralsKtTest.kt
@@ -4,10 +4,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class N36ExtensionFunctionLiteralsKtTest {
-    @Test fun testIsOddAndIsEven() {
+    @Test
+    fun testIsOddAndIsEven() {
         val result = task36()
         assertEquals("The functions 'isOdd' and 'isEven' should be implemented correctly",
                 listOf(false, true, true), result)
-
     }
 }
