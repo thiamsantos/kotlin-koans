85d7c4a936fa42b4bb411be8beea0af49f140a39
Thiago <thia.mdossantos@gmail.com>
2017-12-20 11:24:38 -0200

    resolve 39

--- src/v_builders/n39HtmlBuilders.kt
+++ src/v_builders/n39HtmlBuilders.kt
@@ -1,28 +1,15 @@
 package v_builders
 
-import util.TODO
-import util.doc39
 import v_builders.data.getProducts
 import v_builders.htmlLibrary.*
 
 fun getTitleColor() = "#b9c9fe"
-fun getCellColor(row: Int, column: Int) = if ((row + column) %2 == 0) "#dce4ff" else "#eff2ff"
-
-fun todoTask39(): Nothing = TODO(
-    """
-        Task 39.
-        1) Fill the table with the proper values from products.
-        2) Color the table like a chess board (using getTitleColor() and getCellColor() functions above).
-        Pass a color as an argument to functions 'tr', 'td'.
-        You can call the 'main' function in the 'htmlDemo.kt' to see the rendered table.
-    """,
-    documentation = doc39()
-)
+fun getCellColor(row: Int, column: Int) = if ((row + column) % 2 == 0) "#dce4ff" else "#eff2ff"
 
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr(getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -33,8 +20,16 @@ fun renderProductTable(): String {
                     text("Popularity")
                 }
             }
-            val products = getProducts()
-            todoTask39()
+            val products = getProducts().map { listOf(it.description, it.price, it.popularity) }
+            for ((row, product) in products.withIndex()) {
+                tr {
+                    for ((column, cell) in product.withIndex()) {
+                        td(getCellColor(row, column)) {
+                            text(cell)
+                        }
+                    }
+                }
+            }
         }
     }.toString()
 }
