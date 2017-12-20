baddcab832ac7a43af5f82bf21a1a83ba862fdde
Thiago <thia.mdossantos@gmail.com>
2017-12-20 12:29:00 -0200

    resolve 41 and finish :tada:

--- test/vi_generics/N41GenericFunctionsKtTest.kt
+++ test/vi_generics/N41GenericFunctionsKtTest.kt
@@ -4,13 +4,15 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class N41GenericFunctionsKtTest {
-    @Test fun testPartitionWordsAndLines() {
+    @Test
+    fun testPartitionWordsAndLines() {
         val (words, lines) = listOf("a", "a b", "c", "d e").partitionWordsAndLines()
         assertEquals(listOf("a", "c"), words)
         assertEquals(listOf("a b", "d e"), lines)
     }
 
-    @Test fun testPartitionLettersAndOtherSymbols() {
+    @Test
+    fun testPartitionLettersAndOtherSymbols() {
         val (letters, other) = setOf('a', '%', 'r', '}').partitionLettersAndOtherSymbols()
         assertEquals(setOf('a', 'r'), letters)
         assertEquals(setOf('%', '}'), other)
